def13e2953b492eb656539601c3260f07a90a57d
geofrey <devongeoffreymaina@gmail.com>
2025-03-26 21:42:28 +0300

    Added Supabase Database connection

--- topnotch_back/settings.py
+++ topnotch_back/settings.py
@@ -117,11 +117,31 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': env('SUPABASE_NAME'),
+
+        'USER': env('SUPABASE_USER'),
+
+        'PASSWORD': env('SUPABASE_PASSWORD'),
+
+        'HOST': env('SUPABASE_HOST'),
+
+        'PORT': env('SUPABASE_PORT'),
+
     }
+
 }
 
 
