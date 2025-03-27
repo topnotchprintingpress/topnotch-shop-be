ec85d1e1f7646f33be964a2fe09295f35efca6c8
geofrey <devongeoffreymaina@gmail.com>
2025-03-27 06:54:11 +0300

    Added redirect url

--- topnotch_back/settings.py
+++ topnotch_back/settings.py
@@ -275,13 +275,11 @@ SIMPLE_JWT = {
 
 # CORS settings (allow your Next.js frontend)
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-#     'http://127.0.0.1:3000',
-#     'http://192.168.100:3000',
-#     'http://127.0.0.1:8000'
-# ]
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://topnotchprintingpress.com',
+    'http://www.topnotchprintingpress.com'
+]
 CORS_ALLOW_HEADERS = [
     "content-type",
     "authorization",
