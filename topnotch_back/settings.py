c0ffd195a5be5f851c3337a6e93f841cc67fb1ab
geofrey <devongeoffreymaina@gmail.com>
2025-04-01 19:25:51 +0300

    Added Pagination Size

--- topnotch_back/settings.py
+++ topnotch_back/settings.py
@@ -177,7 +177,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    'PAGE_SIZE': 12,
     'MAX_PAGE_SIZE': 100,
 }
 
